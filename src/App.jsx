872fdbc3504f23f4ab699f1f8bfc44141a06e70f
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-30 13:02:20 -0600

    installed Reactjs

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line react-refresh/only-export-components
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1>Hello!</h1>
+      </header>
+    </div>
+  )
+}
