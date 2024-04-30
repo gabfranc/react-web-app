d03770cec64846d426e9a836d498bf942937a728
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-30 14:16:14 -0600

    descriptions and integers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line react-refresh/only-export-components
+const reactDescriptions = ['REACT', 'Fundamentals', 'Components', 'Props', 'State', 'Lifecycle', 'Hooks', 'Context',]
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+function Header(){
+  const descriptions = reactDescriptions(getRandomInt(2))
+  return (    
+  <div>
+      <header className="header">
+        <h1>Welcome to React!</h1>
+        <p>{descriptions} React is Awesome!</p>
+      </header>
+  </div>)
+}
+
 function App() {
   return (
     <div>
-      <header className="header">
-        <h1>Hello!</h1>
-      </header>
+      <Header />
+      <main>
+        <h2>React to coding</h2>
+      </main>
     </div>
   )
 }
+
+export default App;
